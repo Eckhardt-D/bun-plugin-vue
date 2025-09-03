44719f25d4f9d80a480458aa5f7fca7dedea56e0
Eckhardt-D <eckhardt.dreyer@gmail.com>
2025-09-03 08:24:17 +0200

    perf(vue): do not cache vue resolution replacements, better loop

--- examples/with-build/App.vue
+++ examples/with-build/App.vue
@@ -1,8 +1,10 @@
 <script lang="ts" setup>
+import Counter from './components/Counter.vue';
 import { ref } from 'vue';
 const message = ref('Hello, Vue 3 with TypeScript!');
 </script>
 
 <template>
   <h1>{{ message }}</h1>
+  <Counter />
 </template>
